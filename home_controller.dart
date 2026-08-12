6154cf7fe9bd35ecbfa04fbfd56e7d0b46171c66
សំ សុផល <sophalsam04@gmail.com>
2026-08-12 16:10:44 +0700

    Create home_controller.dart

---
+++ home_controller.dart
@@ -0,0 +1,12 @@
+// create a controller
+
+import 'package:get/get.dart';
+
+class HomeController extends GetxController{
+      var count = 0.obs;
+
+      void increment(){
+          count++;
+      }
+
+}
